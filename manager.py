de33b4b2cc771028e6bb78686d70f42bbfc266e7
hausir <hausir@icloud.com>
2017-07-18 16:21:01 +0800

    [Added] binding host option

--- manager.py
+++ manager.py
@@ -1,25 +1,28 @@
 #!/usr/bin/env python
-# coding=utf-8
+# -*- coding: utf-8 -*-
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import unicode_literals
 
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 
 from tornado.options import define, options
-
 from redisadmin import Application
-
-define("port", default=9000, help="default: 9000, required runserver", type=int)
 
 
 def main():
+    define("host", default='127.0.0.1', help="Set binding host", type=str)
+    define("port", default=9000, help="Set listing port", type=int)
     tornado.options.parse_command_line()
 
-    print('server started. port %s' % options.port)
+    print('server started. visit http://{}:{}'.format(options.host, options.port))
     http_server = tornado.httpserver.HTTPServer(Application(), xheaders=True)
     http_server.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
-
 
 if __name__ == '__main__':
     main()
